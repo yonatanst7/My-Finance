949c02838efbb71ee5f64c362454ae19490b1187
yonatanst7 <104933243+yonatanst7@users.noreply.github.com>
2025-04-12 17:15:16 +0300

    forgot to save changes lol

---
+++ src/firebase/config.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFunctions } from "firebase/functions";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0bJMSFi7py795E0tf-o_fIq3mXJH5hpI",
+  authDomain: "myfinance-47c4d.firebaseapp.com",
+  projectId: "myfinance-47c4d",
+  storageBucket: "myfinance-47c4d.firebasestorage.app",
+  messagingSenderId: "801671499822",
+  appId: "1:801671499822:web:a5dfffd698cfe611c9cfbf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize firebase services
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+const functions = getFunctions(app);
+
+export { auth, db, storage, functions };
